241fa9e76eb88759981958c12b19acf31c34c11e
heiligbasil <basilief@yahoo.com>
2020-02-20 11:08:50 -0800

    Eating Cookies solution via guided lecture

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -20,11 +20,27 @@ import sys
 # how can we recursively call our function such that we move towards one or more of these base cases? As far as
 # performance optimizations go, caching/memoization might be one avenue we could go down? How should we make a cache
 # available to our recursive function through multiple recursive calls?
+# 0: eat 0 cookies 1 time
+# 1: eat 1 cookie 1 time
+# 2: eat 1 cookie 1 time; eat 2 cookies
+# 3: eat 1 cookie 1 time, eat 1 cookie 1 time, eat 1 cookie 1 time; eat 2 cookies 1 time; eat 3 cookies 1 time
 #
 # The cache parameter is here for if you want to implement a solution
 # that is more efficient than the naive recursive solution
 def eating_cookies(n, cache=None):
-    pass
+    # Start with the base case
+    if n < 0:
+        return 0
+    if n == 0:
+        return 1
+    elif cache and cache[n] > 0:
+        return cache[n]
+    else:
+        if cache is None:
+            cache = {}
+        value = eating_cookies(n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
+        cache[n] = value
+        return value
 
 
 if __name__ == "__main__":
