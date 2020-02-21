13aeb917f2a10911517004f0e2058b240a53417a
heiligbasil <basilief@yahoo.com>
2020-02-20 20:15:06 -0800

    Alternate solution for Stock Prices from classmate

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -46,6 +46,23 @@ def find_max_profit(prices):
     return maximum_profit
 
 
+def find_max_profit_alternate(prices):
+    current_min_profit = prices[0]  # Initialize to the first value in our array
+    current_max_profit = prices[1] - prices[
+        0]  # Max profit is defined as the next value in the array minus the previous value. No shorting allowed.
+    for i in range(1, len(prices)):  # Loop through our prices array starting at the 2nd index
+        if prices[i] < current_min_profit:  # If the index is smaller than our smallest current value, swap
+            current_min_profit = prices[i]  # Replace our smallest value
+        elif prices[
+            i] - current_min_profit > current_max_profit:  # If the current index minus the current smallest value is greater than current max, replace our current max.
+            current_max_profit = prices[
+                                     i] - current_min_profit  # Max profit is found by subtracting the current smallest value from each index as we loop.
+    return current_max_profit
+
+
+print(f'{find_max_profit_alternate([1050, 270, 1540, 3800, 2])}')
+print(f'{find_max_profit_alternate([10, 2700, 150, 300, 277, 1100, 1500, 5000, 1700])}')
+
 if __name__ == '__main__':
     # This is just some code to accept inputs from the command line
     parser = argparse.ArgumentParser(description='Find max profit from prices.')
